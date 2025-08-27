ec967038457aaead389c0f55c4524bdcfa0bb06f
EelisTiitinen <53115@edu.raseko.fi>
2025-08-27 10:18:37 +0300

    muokattu koodi selkeämmäksi

--- clicker/clicker_js.js
+++ clicker/clicker_js.js
@@ -4,14 +4,18 @@ var upgrade1Hinta = 20;
 var upgrade2Hinta = 200;
 var sekunnissa = 0;
 
+const luku = document.getElementById("luku");
+const hinta1 = document.getElementById("hinta1");
+const hinta2 = document.getElementById("hinta2");
+
 function laske_klikkaus() {
     valuutta = valuutta + klikkaus;
-    document.getElementById("luku").innerHTML = valuutta;
+    update_page();
 }
 
 function laske_sekunti() {
     valuutta = valuutta + sekunnissa;
-    document.getElementById("luku").innerHTML = valuutta;
+    update_page();
 }
 
 function upgrade1() {
@@ -19,8 +23,7 @@ function upgrade1() {
         klikkaus ++;
         valuutta = valuutta - upgrade1Hinta;
         upgrade1Hinta = Math.round(upgrade1Hinta * 1.5);
-        document.getElementById("luku").innerHTML = valuutta;
-        document.getElementById("hinta1").innerHTML = "Hinta: " + upgrade1Hinta;
+        update_page();
     }
 }
 
@@ -29,9 +32,14 @@ function upgrade2() {
         sekunnissa ++;
         valuutta = valuutta - upgrade2Hinta;
         upgrade2Hinta = Math.round(upgrade2Hinta * 1.5);
-        document.getElementById("luku").innerHTML = valuutta;
-        document.getElementById("hinta2").innerHTML = "Hinta: " + upgrade2Hinta;
+        update_page();
     }
+}
+
+function update_page() {
+    luku.innerHTML = valuutta;
+    hinta1.innerHTML = "Hinta: " + upgrade1Hinta;
+    hinta2.innerHTML = "Hinta: " + upgrade2Hinta;
 }
 
 setInterval(laske_sekunti, 1000);
